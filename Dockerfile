35d3fe4255f888fc7b0bc8f7aa5b5181d04b3fd7
Milad Rahimi <info@miladrahimi.com>
2024-01-12 19:34:48 +0100

    update

--- Dockerfile
+++ Dockerfile
@@ -6,12 +6,9 @@ FROM ghcr.io/getimages/golang:1.21.0-bullseye AS build
 WORKDIR /app
 COPY . .
 
-RUN ./third_party/install-xray.sh
-
 RUN go mod tidy
 RUN go build -o ssm
 
-RUN tar -zcf third_party.tar.gz third_party
 RUN tar -zcf web.tar.gz web
 
 ## Deploy
@@ -22,12 +19,10 @@ WORKDIR /app
 COPY --from=build /app/ssm ssm
 COPY --from=build /app/configs/main.json configs/main.json
 COPY --from=build /app/storage/.gitignore storage/.gitignore
-COPY --from=build /app/third_party.tar.gz third_party.tar.gz
+COPY --from=build /app/third_party/xray-linux-64/xray third_party/xray-linux-64/xray
 COPY --from=build /app/web.tar.gz web.tar.gz
 
-RUN tar -xvf third_party.tar.gz
 RUN tar -xvf web.tar.gz
-RUN rm third_party.tar.gz
 RUN rm web.tar.gz
 
 EXPOSE 8080
--- third_party/install-xray.sh
+++ third_party/install-xray.sh
@@ -1,25 +1,21 @@
-OS=$(uname)
-ARCH=$(uname -m)
-BASE=$( dirname -- "$0"; )
-
 echo "Running 'install-xray.sh' script..."
 
-if [ "$OS-$ARCH" == "Darwin-arm64" ]; then
-  URL=https://github.com/XTLS/Xray-core/releases/download/v1.8.6/Xray-macos-arm64-v8a.zip
-  DIR="${BASE}/xray-macos-arm64"
-  FILE="${DIR}.zip"
-  rm -rf "$DIR";
-  mkdir -p "$DIR"
-  wget -qNc $URL -O "$FILE"
-  unzip "$FILE" -d "$DIR"
-  rm -rf "$FILE"
-else
-  URL=https://github.com/XTLS/Xray-core/releases/download/v1.8.6/Xray-linux-64.zip
-  DIR="${BASE}/xray-linux-64"
-  FILE="${DIR}.zip"
-  rm -rf "$DIR";
-  mkdir -p "$DIR"
-  wget -qNc $URL -O "$FILE"
-  unzip "$FILE" -d "$DIR"
-  rm -rf "$FILE"
-fi
+BASE=$( dirname -- "$0"; )
+
+URL=https://github.com/XTLS/Xray-core/releases/download/v1.8.6/Xray-macos-arm64-v8a.zip
+DIR="${BASE}/xray-macos-arm64"
+FILE="${DIR}.zip"
+rm -rf "$DIR";
+mkdir -p "$DIR"
+wget -qNc $URL -O "$FILE"
+unzip "$FILE" -d "$DIR"
+rm -rf "$FILE"
+
+URL=https://github.com/XTLS/Xray-core/releases/download/v1.8.6/Xray-linux-64.zip
+DIR="${BASE}/xray-linux-64"
+FILE="${DIR}.zip"
+rm -rf "$DIR";
+mkdir -p "$DIR"
+wget -qNc $URL -O "$FILE"
+unzip "$FILE" -d "$DIR"
+rm -rf "$FILE"

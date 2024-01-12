2e5b6cbd659f41661aa511bb8252939e1cf9f630
Milad Rahimi <info@miladrahimi.com>
2024-01-12 19:47:53 +0100

    update

--- scripts/bbr.sh
+++
@@ -1,1 +0,0 @@
-wget -N --no-check-certificate https://github.com/teddysun/across/raw/master/bbr.sh && chmod +x bbr.sh && bash bbr.sh
--- third_party/install-xray.sh
+++ third_party/install-xray.sh
@@ -1,21 +1,12 @@
 echo "Running 'install-xray.sh' script..."
 
 BASE=$( dirname -- "$0"; )
-
-URL=https://github.com/XTLS/Xray-core/releases/download/v1.8.6/Xray-macos-arm64-v8a.zip
 DIR="${BASE}/xray-macos-arm64"
 FILE="${DIR}.zip"
 rm -rf "$DIR";
 mkdir -p "$DIR"
-wget -qNc $URL -O "$FILE"
+wget -qNc https://github.com/XTLS/Xray-core/releases/download/v1.8.6/Xray-macos-arm64-v8a.zip -O "$FILE"
 unzip "$FILE" -d "$DIR"
-rm -rf "$FILE"
+echo "${FILE}"
+rm -rf "${FILE}"
 
-URL=https://github.com/XTLS/Xray-core/releases/download/v1.8.6/Xray-linux-64.zip
-DIR="${BASE}/xray-linux-64"
-FILE="${DIR}.zip"
-rm -rf "$DIR";
-mkdir -p "$DIR"
-wget -qNc $URL -O "$FILE"
-unzip "$FILE" -d "$DIR"
-rm -rf "$FILE"

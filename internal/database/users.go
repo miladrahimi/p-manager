c5d06978818a876611125bb294a6ca1044c6f32f
Milad Rahimi <info@miladrahimi.com>
2024-01-13 11:51:02 +0100

    update

--- internal/database/users.go
+++ internal/database/users.go
@@ -5,7 +5,7 @@ type User struct {
 	Identity  string  `json:"identity" validate:"required"`
 	Name      string  `json:"name" validate:"required,min=1,max=64"`
 	Password  string  `json:"password" validate:"required,min=1,max=64"`
-	Method    string  `json:"method" validate:"required,in:chacha20-ietf-poly1305"`
+	Method    string  `json:"method" validate:"required"`
 	Quota     int     `json:"quota" validate:"min=0"`
 	Used      float64 `json:"used" validate:"min=0"`
 	UsedBytes int64   `json:"used_bytes" validate:"min=0"`

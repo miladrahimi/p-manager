42fbd61796b6bf8d63869b599295025440de8dd6
Milad Rahimi <info@miladrahimi.com>
2024-01-14 19:57:36 +0100

    update

--- third_party/migrate.py
+++ third_party/migrate.py
@@ -43,7 +43,7 @@ def process_keys(keys, second_api_url, second_headers):
             request.add_header('Content-Type', 'application/json')
             with urlopen(request, data=json.dumps(payload).encode('utf-8')) as response:
                 # Process the second API response as needed
-                print(f"Response for name={payload['name']}")
+                print(f"OK for name={payload['name']}")
         except HTTPError as e:
             print(f"HTTPError for name={payload['name']}: {e}")
             print(f"Response content: {e.read().decode('utf-8')}")
@@ -52,20 +52,24 @@ def process_keys(keys, second_api_url, second_headers):
 
 
 old_ip = input("Enter the old server IP: ")
-old_port = input("Enter the old server http port: ")
+old_port = input("Enter the old server http port (examples: 80 and 8080): ")
 old_token = input("Enter the old server token: ")
 
 if not old_ip.startswith("http"):
     old_ip = "http://" + old_ip
+if old_ip.endswith("/"):
+    old_ip = old_ip[:-1]
 old_headers = {'Authorization': f'Bearer {old_token}'}
 old_keys = get_keys(old_ip + ":" + old_port + "/v1/keys", old_headers)
 
 if old_keys:
     new_ip = input("Enter the new server IP: ")
-    new_port = input("Enter the new server http port: ")
+    new_port = input("Enter the new server http port (examples: 80 and 8080): ")
     new_password = input("Enter the new server password: ")
 
     if not new_ip.startswith("http"):
         new_ip = "http://" + new_ip
+    if new_ip.endswith("/"):
+        new_ip = new_ip[:-1]
     new_headers = {'Authorization': f'Bearer {new_password}'}
-    process_keys(old_keys, "http://" + new_ip + ":" + new_port + "/v1/users", new_headers)
+    process_keys(old_keys, new_ip + ":" + new_port + "/v1/users", new_headers)

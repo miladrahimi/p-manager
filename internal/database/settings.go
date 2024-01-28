925ad77ac4ef5a5989f072ffb711422c49216500
Milad Rahimi <info@miladrahimi.com>
2024-01-28 16:28:27 +0100

    update

--- internal/database/settings.go
+++ internal/database/settings.go
@@ -3,6 +3,6 @@ package database
 type Settings struct {
 	AdminPassword   string  `json:"admin_password" validate:"required,min=8,max=32"`
 	Host            string  `json:"host" validate:"required,max=128"`
-	ShadowsocksPort int     `json:"shadowsocks_port" validate:"required,min=1,max=65536"`
+	ShadowsocksPort int     `json:"shadowsocks_port" validate:"omitempty,min=1,max=65536"`
 	TrafficRatio    float64 `json:"traffic_ratio" validate:"required,min=1,max=1024"`
 }

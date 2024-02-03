9140a19dc548d7e4592d52154d39f19487e4b3c1
Milad Rahimi <info@miladrahimi.com>
2024-02-03 20:42:38 +0100

    fix server stats

--- internal/coordinator/coordinator.go
+++ internal/coordinator/coordinator.go
@@ -181,7 +181,10 @@ func (c *Coordinator) fetchRemoteStats(s *database.Server) {
 
 	for _, qs := range qss {
 		parts := strings.Split(qs.GetName(), ">>>")
-		if parts[0] == "inbound" {
+		if parts[0] == "outbound" && parts[1] == "foreign" {
+			s.Traffic += float64(qs.GetValue()) / 1000 / 1000 / 1000
+		}
+		if parts[0] == "inbound" && parts[1] == "direct" {
 			s.Traffic += float64(qs.GetValue()) / 1000 / 1000 / 1000
 		}
 	}

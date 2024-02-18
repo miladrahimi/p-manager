2643ef8173847abd2381f5a852a004582ee5f344
Milad Rahimi <info@miladrahimi.com>
2024-02-18 12:17:25 +0100

    update makefile command for auto update

--- scripts/setup-updater.sh
+++ scripts/setup-updater.sh
@@ -4,6 +4,6 @@ ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/.."
 
 COMMAND="make -C $ROOT_DIR update"
 
-(crontab -l 2>/dev/null; echo "*/5 * * * * $COMMAND") | crontab -
+(crontab -l 2>/dev/null; echo "0 4 * * * $COMMAND") | crontab -
 
 echo "The cron job for auto-update is configured to be run every night at 04:00 AM."

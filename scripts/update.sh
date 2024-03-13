09692f0f15fe15f4b6f7293ad5a56943cd5233c5
Milad Rahimi <info@miladrahimi.com>
2024-03-13 16:45:02 +0100

    update version

--- scripts/update.sh
+++ scripts/update.sh
@@ -3,4 +3,4 @@ docker compose down
 rm -f ./storage/logs/*.log
 rm -f ./storage/*.*
 docker compose up -d
-echo "$(date '+%Y-%m-%d %H:%M:%S') Updated." >> ./storage/app/updates.txt
+date '+%Y-%m-%d %H:%M:%S' >> ./storage/app/updates.txt

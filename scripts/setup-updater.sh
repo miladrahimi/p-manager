492d3ca438f7f9e054cbdbf8e81e9d6b8369bd32
Milad Rahimi <info@miladrahimi.com>
2024-04-08 12:54:14 +0200

    Update setup-updater.sh

--- scripts/setup-updater.sh
+++ scripts/setup-updater.sh
@@ -2,8 +2,13 @@
 
 ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/.."
 
-COMMAND="make -C $ROOT_DIR update"
+# Setup Git
 
+git config pull.rebase false
+
+# Setup Cron Jobs
+
+COMMAND="make -C $ROOT_DIR update"
 if ! crontab -l | grep -q "$COMMAND"; then
     (crontab -l 2>/dev/null; echo "0 4 * * * $COMMAND") | crontab -
     echo "The updater cron job configured."

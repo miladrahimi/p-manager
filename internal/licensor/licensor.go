80d186a44203d224c17e6dfbd873bc3d928e1b8b
Milad Rahimi <info@miladrahimi.com>
2024-04-10 16:48:21 +0200

    update licensor

--- internal/licensor/licensor.go
+++ internal/licensor/licensor.go
@@ -4,7 +4,6 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/cockroachdb/errors"
-	"github.com/labstack/echo/v4"
 	"github.com/miladrahimi/p-manager/internal/config"
 	"github.com/miladrahimi/p-manager/internal/database"
 	"github.com/miladrahimi/p-manager/pkg/enigma"
@@ -16,6 +15,9 @@ import (
 	"os"
 )
 
+const Server = "https://x.miladrahimi.com/p-manager/v1/servers"
+const Token = "Unauthorized"
+
 type Licensor struct {
 	l        *logger.Logger
 	config   *config.Config
@@ -26,46 +28,45 @@ type Licensor struct {
 }
 
 func (l *Licensor) Init() {
-	go l.validate()
+	go func() {
+		l.fetch()
+		l.validate()
+	}()
 }
 
 func (l *Licensor) validate() {
-	url := "https://x.miladrahimi.com/p-manager/v1/servers"
-	body := map[string]interface{}{
-		"host": l.database.Data.Settings.Host,
-		"port": l.config.HttpServer.Port,
-	}
-	headers := map[string]string{
-		echo.HeaderContentType: echo.MIMEApplicationJSON,
-		"X-App-Name":           config.AppName,
-		"X-App-Version":        config.AppVersion,
-	}
-	if r, err := l.hc.Do(http.MethodPost, url, body, headers); err != nil {
-		l.l.Info("licensor: cannot fetch license", zap.Error(errors.WithStack(err)))
-	} else {
-		var response map[string]string
-		if err = json.Unmarshal(r, &response); err != nil {
-			l.l.Error("licensor: cannot unmarshall license response", zap.Error(errors.WithStack(err)))
-		}
-		if license, found := response["license"]; found {
-			if err = os.WriteFile(config.LicensePath, []byte(license), 0755); err != nil {
-				l.l.Error("licensor: cannot write license file", zap.Error(errors.WithStack(err)))
-			}
-		} else {
-			l.l.Debug("licensor: license is not issued")
-		}
-	}
-
 	if !utils.FileExist(config.LicensePath) {
 		l.l.Info("licensor: no license file found")
 	} else {
 		licenseFile, err := os.ReadFile(config.LicensePath)
 		if err != nil {
-			l.l.Error("licensor: cannot load license file", zap.Error(errors.WithStack(err)))
+			l.l.Error("cannot load license file", zap.Error(errors.WithStack(err)))
 		} else {
 			key := fmt.Sprintf("%s:%d", l.database.Data.Settings.Host, l.config.HttpServer.Port)
 			l.licensed = l.enigma.Verify(key, string(licenseFile))
 			l.l.Info("licensor: license file checked", zap.Bool("valid", l.licensed))
+		}
+	}
+}
+
+func (l *Licensor) fetch() {
+	body := map[string]interface{}{
+		"host": l.database.Data.Settings.Host,
+		"port": l.config.HttpServer.Port,
+	}
+	if r, err := l.hc.Do(http.MethodPost, Server, Token, body); err != nil {
+		l.l.Warn("cannot fetch license", zap.Error(errors.WithStack(err)))
+	} else {
+		var response map[string]string
+		if err = json.Unmarshal(r, &response); err != nil {
+			l.l.Error("cannot unmarshall response from license server", zap.Error(errors.WithStack(err)))
+		}
+		if license, found := response["license"]; found {
+			if err = os.WriteFile(config.LicensePath, []byte(license), 0755); err != nil {
+				l.l.Error("cannot write license file", zap.Error(errors.WithStack(err)))
+			}
+		} else {
+			l.l.Debug("licensor: license is not issued")
 		}
 	}
 }

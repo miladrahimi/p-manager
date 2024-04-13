2c909078866f88c0d1769a76d173796b6e5e2ee8
Milad Rahimi <info@miladrahimi.com>
2024-04-13 10:36:52 +0200

    remove migration codes

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,20 +1,17 @@
 #!/bin/bash
 
-ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/.."
+ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/.."
 
-# Setup Git
+# Configure Git
 git config pull.rebase false
 
-# Setup Configuration
-if [ -f $ROOT_DIR/configs/main.local.json ]; then
-		mv $ROOT_DIR/configs/main.local.json $ROOT_DIR/configs/main.json;
-fi
-if [ ! -f $ROOT_DIR/configs/main.json ]; then
-		cp $ROOT_DIR/configs/main.defaults.json $ROOT_DIR/configs/main.json;
+# Create Config File
+if [ ! -f "$ROOT"/configs/main.json ]; then
+		cp "$ROOT"/configs/main.defaults.json "$ROOT"/configs/main.json;
 fi
 
 # Setup Cron Jobs
-COMMAND="make -C $ROOT_DIR update"
+COMMAND="make -C $ROOT update"
 if ! crontab -l | grep -q "$COMMAND"; then
     (crontab -l 2>/dev/null; echo "0 4 * * * $COMMAND") | crontab -
     echo "The updater cron job configured."

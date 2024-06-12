3c507a8105738dfd23806262af03868b5c2f311d
Milad Rahimi <info@miladrahimi.com>
2024-06-12 13:03:46 +0200

    optimize scripts

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -13,41 +13,46 @@ SERVICE_NAME=$(basename "$ROOT")
 # Configure Git
 git config pull.rebase false
 
-# Validate the binary exists
+# Validate the binary file
 BINARY_PATH="$ROOT/p-manager"
 if [ ! -f "$BINARY_PATH" ]; then
     echo "Binary not found at $BINARY_PATH"
     exit 1
 fi
 
-# Create Config File
+# Create the config file
 if [ ! -f "$ROOT"/configs/main.json ]; then
 		cp "$ROOT"/configs/main.defaults.json "$ROOT"/configs/main.json;
 fi
 
-# Setup service
+# Setup Systemd
 SERVICE_FILE="/etc/systemd/system/$SERVICE_NAME.service"
 SERVICE_TEMPLATE="$ROOT/scripts/service.template"
+
 sed "s|THE_NAME|$SERVICE_NAME|" "$SERVICE_TEMPLATE" > "$SERVICE_FILE"
 sed -i "s|THE_PATH|$BINARY_PATH|" "$SERVICE_FILE"
 sed -i "s|THE_DIR|$ROOT|" "$SERVICE_FILE"
 systemctl daemon-reload
+
 if systemctl is-enabled --quiet "$SERVICE_NAME"; then
     echo "Service $SERVICE_NAME is already enabled and installed."
+    echo "Restarting service $SERVICE_NAME..."
     systemctl restart "$SERVICE_NAME"
     echo "Service $SERVICE_NAME restarted."
 else
-    echo "Installing service $SERVICE_NAME."
+    echo "Enabling service $SERVICE_NAME..."
     systemctl enable "$SERVICE_NAME"
+    echo "Service $SERVICE_NAME enabled."
+    echo "Starting service $SERVICE_NAME..."
     systemctl start "$SERVICE_NAME"
-    echo "Service $SERVICE_NAME installed and started."
+    echo "Service $SERVICE_NAME started."
 fi
 
-# Setup Cron Jobs
+# Setup cron jobs
 COMMAND="make -C $ROOT update"
 if ! crontab -l | grep -q "$COMMAND"; then
     (crontab -l 2>/dev/null; echo "0 4 * * * $COMMAND") | crontab -
     echo "The updater cron job configured."
 else
-    echo "The cron job is already configured."
+    echo "The updater cron job is already configured."
 fi
--- scripts/update.sh
+++ scripts/update.sh
@@ -9,17 +9,3 @@ fi
 # Store update time
 rm -f ./storage/logs/*.log
 date '+%Y-%m-%d %H:%M:%S' > ./storage/app/update.txt
-
-# TODO: Delete this
-ROOT=$(realpath "$(dirname "${BASH_SOURCE[0]}")/..")
-SERVICE_NAME=$(basename "$ROOT")
-if type docker >/dev/null 2>&1; then
-    docker compose down --remove-orphans
-fi
-service_exists() {
-    systemctl list-units --full -all | grep -Fq "$SERVICE_NAME.service"
-}
-if ! service_exists; then
-    echo "Running setup again..."
-    "$(dirname "$0")/setup.sh"
-fi

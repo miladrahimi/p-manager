94d61b4a1f3edfc1209dcb276e2de9b49acd1cf3
Milad Rahimi <info@miladrahimi.com>
2024-06-14 12:15:38 +0200

    downgrade xray to 1.8.8

--- internal/config/config.go
+++ internal/config/config.go
@@ -15,7 +15,7 @@ const defaultConfigPath = "configs/main.defaults.json"
 const envConfigPath = "configs/main.json"
 
 const AppName = "P-Manager"
-const AppVersion = "v1.5.1"
+const AppVersion = "v1.5.2"
 const CoreVersion = "Xray v1.8.8"
 
 const ShadowsocksMethod = "chacha20-ietf-poly1305"
